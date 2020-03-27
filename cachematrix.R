f388fd894fc2e9a994aa3020ce3c93fcb654ba87
unknown <jerzy.kwiatkowski@yahoo.pl>
2020-03-27 18:49:45 +0100

    home work ver. 2.0

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,3 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = numeric()) {
     m <- NULL
@@ -19,8 +15,6 @@ makeCacheMatrix <- function(x = numeric()) {
 }
 
 
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
     m <- x$getinv()
